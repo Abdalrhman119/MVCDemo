515f58ae765f66634580a80cae06229f58644ba9
Abdalrhman <wboody68@gmail.com>
2025-04-04 15:45:19 +0200

    Add project files.

---
+++ Demo.BusinessLogic/Services/DepartmentService.cs
@@ -0,0 +1,21 @@
+﻿using Demo.DataAccess.Repositories;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Demo.BusinessLogic.Services
+{
+    internal class DepartmentService
+    {
+        private readonly IDepartmentRepository _DepartmentRepository;
+
+        public DepartmentService(IDepartmentRepository departmentRepository)
+        {
+            _DepartmentRepository = departmentRepository;
+        }
+
+
+    }
+}
---
+++ Demo.Peresentation/Program.cs
@@ -0,0 +1,50 @@
+using Demo.DataAccess.Contexts;
+using Demo.DataAccess.Repositories;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Demo.Peresentation
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var builder = WebApplication.CreateBuilder(args);
+
+            #region Add services to the container
+            builder.Services.AddControllersWithViews();
+            builder.Services.AddDbContext<AppDbContext>(options => {
+                options.UseSqlServer(builder.Configuration["ConnectionString:DefaultConnection"]);
+                //options.UseSqlServer(builder.Configuration.GetSection("ConnectionString")["DefaultConnection"]);
+                //options.UseSqlServer(builder.Configuration.GetConnectionString("ConnectionString"));
+            
+            });
+            builder.Services.AddScoped<IDepartmentRepository,DepartmentRepository>();
+            #endregion
+
+            var app = builder.Build();
+
+            #region Configure the HTTP request pipeline
+            if (!app.Environment.IsDevelopment())
+            {
+                app.UseExceptionHandler("/Home/Error");
+                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+                app.UseHsts();
+            }
+
+            app.UseHttpsRedirection();
+            app.UseStaticFiles();
+
+            app.UseRouting();
+
+            app.UseAuthorization();
+
+            app.MapControllerRoute(
+                name: "default",
+                pattern: "{controller=Home}/{action=Index}/{id?}");
+
+            #endregion
+            app.Run();
+        }
+    }
+}
